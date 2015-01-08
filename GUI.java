1d4d96f906fa3da514469d67431b49208a94e06f
Michaela Papallo <michaelapapallo@gmail.com>
2015-01-07 20:47:04 -0500

    trying to figure out gui split panes

--- GUI.java
+++ GUI.java
@@ -5,42 +5,57 @@ public class GUI extends JFrame implements ActionListener{
     
     private Student player;
     private Container window;
+    private JSplitPane pane;
+    private JPanel stats, interact;
     private JPanel pane1, pane2, pane3, pane4;
-    private JLabel stress, knowledge, energy;
+    private JLabel stress, knowledge, energy, story;
 
     public GUI(){
 	this.setTitle("Stuyvesant Finals Week Simulator");
-	this.setSize(800, 800);
+	this.setSize(900, 900);
 	this.setLocation(100, 100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	window = this.getContentPane();
-	window.setLayout(new GridBagLayout());
+	window.setLayout(new FlowLayout());
 
-	pane1 = new JPanel(new BorderLayout());
-	pane1.setBorder(BorderFactory.createLoweredBevelBorder());
-	pane2 = new JPanel(new BorderLayout());
-	pane2.setBorder(BorderFactory.createLoweredBevelBorder());
-	pane3 = new JPanel(new BorderLayout());
-	pane3.setBorder(BorderFactory.createLoweredBevelBorder());
+	stats = new JPanel(new BorderLayout());
+	stats.setSize(400, 400);
+	stats.setBorder(BorderFactory.createRaisedBevelBorder());
+	interact = new JPanel(new BorderLayout());
+	interact.setSize(400, 400);
+	interact.setBorder(BorderFactory.createRaisedBevelBorder());
+	Dimension minSize = new Dimension(100, 100);
+	stats.setMinimumSize(minSize);
+	interact.setMinimumSize(minSize);
 
-	//window.add(pane1);
-	//window.add(pane2);
-	//window.add(pane3);
-	//window.add(pane4);
+	pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, stats, interact);
+	pane.setOneTouchExpandable(true);
+	pane.setDividerLocation(100);
+	pane.setSize(800, 800);
+	pane.setResizeWeight(0.5);
+	
+	story = new JLabel("It's Finals Week!");
+	story.setBorder(BorderFactory.createLineBorder(Color.black));
+	interact.add(story, BorderLayout.CENTER);
+
+	stress = new JLabel();
+	stress.setBorder(BorderFactory.createLoweredBevelBorder());
+	knowledge = new JLabel();
+	knowledge.setBorder(BorderFactory.createLoweredBevelBorder());
+	energy = new JLabel();	
+	energy.setBorder(BorderFactory.createLoweredBevelBorder());
     }
 
     public void initialize(Student s){
 	player = s;
-	stress = new JLabel("stress: " + player.getStress());
-	knowledge = new JLabel("knowledge: " + player.getKnow());
-	energy = new JLabel("energy: " + player.getEnergy());
-	pane1.add(stress);
-	pane2.add(knowledge);
-	pane3.add(energy);
-	window.add(pane1);
-	window.add(pane2);
-	window.add(pane3);
+	stress.setText("stress: " + player.getStress());
+	knowledge.setText("knowledge: " + player.getKnow());
+	energy.setText("energy: " + player.getEnergy());
+	stats.add(stress, BorderLayout.PAGE_START);
+	stats.add(knowledge, BorderLayout.CENTER);
+	stats.add(energy, BorderLayout.PAGE_END);
+	window.add(pane);
     }
 
     public void updateStress(int s){
