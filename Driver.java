aa7b6b684820d32a8b95f2ce9f8c9276b7ae6bff
Michaela Papallo <michaelapapallo@gmail.com>
2015-01-07 19:39:33 -0500

    updated Driver to be able to initialize a character

--- Driver.java
+++ Driver.java
@@ -1,11 +1,48 @@
+import java.util.*;
 public class Driver{
-    
-    public static void main(String[]args){
-        GUI G = new GUI();
-	G.setVisible(true);
 
-	Freshman f = new Freshman("Fiona");
-	Student j = new Junior("Joey");
+    public static void main(String[]args){
+	Student player;
+	GUI G;
+	
+	String name = playerChooseName();
+	player = playerChooseDifficulty(name);
+
+	G = new GUI();
+	G.initialize(player);
+	G.setVisible(true);
     }
 
+    public static String playerChooseName(){
+	Scanner in = new Scanner(System.in);
+	System.out.println("Welcome to the Stuyvesant Finals Week Simulator. What is your name?");
+	String name = in.nextLine();
+	return name;
+    }
+
+    public static Student playerChooseDifficulty(String name){
+	Student player;
+	Scanner in = new Scanner(System.in);
+	System.out.println("Nice to meet you! Now, what grade are you in?");
+	boolean done;
+	do{
+	    done = true;
+	    System.out.println("A - Freshman (Easy Street)\nB - Sophomore\nC - Junior\nD - Senior (Hell Week)");
+	    String input = in.nextLine();
+	    if (input.toLowerCase().equals("a")){
+		player = new Freshman();;
+	    }else if (input.toLowerCase().equals("b")){
+		player = new Sophomore();
+	    }else if (input.toLowerCase().equals("c")){
+		player = new Junior();
+	    }else if (input.toLowerCase().equals("d")){
+		player = new Senior();
+	    }else{
+		player = new Freshman();
+		done = false;
+	    }
+	}while(!done);
+	player.setName(name);
+	return player;
+    }
 }
