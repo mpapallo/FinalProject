bf6b837bcff6676436de0dfb25e0320384c280c2
mpapallo <michaelapapallo@gmail.com>
2015-01-09 12:30:35 -0500

    transitioning driver to gui, still not done

--- GUI.java
+++ GUI.java
@@ -7,7 +7,10 @@ public class GUI extends JFrame implements ActionListener{
     private Container window;
     private JSplitPane pane;
     private JPanel stats, interact;
-    private JLabel stress, knowledge, energy, story;
+    private JLabel intro, stress, knowledge, energy, story;
+    private JTextField llamo;
+    private JButton begin;
+    private JRadioButton f, s, jr, sr;
 
     public GUI(){
 	this.setTitle("Stuyvesant Finals Week Simulator");
@@ -22,7 +25,16 @@ public class GUI extends JFrame implements ActionListener{
 	stats.setLayout(b);
 	stats.setBorder(BorderFactory.createRaisedBevelBorder());
 
-	interact = new JPanel(new BorderLayout());
+	stress = new JLabel();
+	stress.setBorder(BorderFactory.createLoweredBevelBorder());
+	knowledge = new JLabel();
+	knowledge.setBorder(BorderFactory.createLoweredBevelBorder());
+	energy = new JLabel();	
+	energy.setBorder(BorderFactory.createLoweredBevelBorder());
+
+	interact = new JPanel();
+	BoxLayout b2 = new BoxLayout(interact, BowLayout.Y_AXIS);
+	interact.setLayout(b2);
 	interact.setBorder(BorderFactory.createRaisedBevelBorder());
 
 	Dimension minSize = new Dimension(50,50);
@@ -37,26 +49,47 @@ public class GUI extends JFrame implements ActionListener{
 	pane.setSize(500,500);
 	pane.setResizeWeight(0.5);
 	
-	story = new JLabel("It's Finals Week!");
-	interact.add(story, BorderLayout.CENTER);
+	intro = new JLabel("Welcome to the Stuyvesant Finals Week Simulator!\nWhat is your name?");
+	llamo = new JTextField("Your name here");
+	interact.add(intro);
+	interact.add(llamo);
+	addDifficultyOptions();
+	begin = new JButton("Next Step");
+	begin.setActionCommand("startGame");
+	interact.add(begin);
 
-	stress = new JLabel();
-	stress.setBorder(BorderFactory.createLoweredBevelBorder());
-	knowledge = new JLabel();
-	knowledge.setBorder(BorderFactory.createLoweredBevelBorder());
-	energy = new JLabel();	
-	energy.setBorder(BorderFactory.createLoweredBevelBorder());
+	window.add(pane, BorderLayout.CENTER);
     }
 
-    public void initialize(Student s){
-	player = s;
+    public void addDifficultyOptions(){
+	f = new JRadioButton("Freshman (Easy Street)");
+	f.setActionCommand("Freshman");
+	f.addActionListener(this);
+	s = new JRadioButton("Sophomore");
+	s.setActionCommand("Sophomore");
+	s.addActionListener(this);	
+	jr = new RadioButton("Junior");
+	jr.setActionCommand("Junior");	
+	jr.addActionListener(this);	
+	sr = new JRadioButton("Senior (Hell Week)");
+	sr.setActionCommand("Senior");
+	sr.addActionListener(this);	
+	ButtonGroup difficulty = new ButtonGroup();
+
+	interact.add(difficulty);
+    }
+
+    public void initialize(String name){
+	//instantiate the player
+
 	stress.setText("stress: " + player.getStress());
 	knowledge.setText("knowledge: " + player.getKnow());
 	energy.setText("energy: " + player.getEnergy());
 	stats.add(stress);
 	stats.add(knowledge);
 	stats.add(energy);
-	window.add(pane, BorderLayout.CENTER);
+	story = new JLabel("It's Finals Week!");
+	interact.add(story, BorderLayout.CENTER);
     }
 
     public void updateStress(int s){
@@ -71,6 +104,9 @@ public class GUI extends JFrame implements ActionListener{
 
     public void actionPerformed(ActionEvent e){
 	String action = e.getActionCommand();
+	if (action.equals("startGame")){
+	   
+	}
     }
 
 }
